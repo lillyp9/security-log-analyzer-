4abbce7af61932b92be3d5288b07f904d74085ad
lillyp9 <lillyperez9@icloud.com>
2026-03-25 19:21:09 -0400

    Add analyze_logs.py : parse logs , extraxt Ip that are susipisoud with high amount of attempts indictaing brute force attack.

---
+++ scripts/analyze_logs.py
@@ -0,0 +1,66 @@
+import pandas as pd 
+import re
+
+#open the file (txt.file)
+file_path = 'data/ssh_logs.txt'
+
+try:
+    with open( file_path, 'r') as file:
+        logs = file.readlines()
+        print(len(logs))
+    #catch error:
+except FileNotFoundError:
+    print(f"Error: The file'{file_path}'was not found.")
+except Exception as e:
+    print(f"An error occured: {e}")
+
+#Parse the log file 
+#extract 1 IP 
+
+def parse_log_line(line):
+    ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    match = re.search(ip_pattern, line)
+    if match: 
+        return match.group()
+    else:
+        return None
+#tesing if code was able to etxraxt the logs 
+#test_line = logs[0]
+#print(f"Test line: {test_line.strip()}")
+#print(f"Extracted IP: {parse_log_line(test_line)}")
+
+#Extract all 430 log lines 
+def parse_all_logs(logs):
+    records = [] #placeholder
+    for line in logs: #loop in every line 
+        ip = parse_log_line(line) #call parse_log_line in each line 
+        if ip: 
+            records.append({ #add to records 
+                "ip": ip, #list of dict 
+                "raw_line": line  #list of dict 
+            })
+    return pd.DataFrame(records) # create a dataframe 
+
+#show dataframe entries 
+df = parse_all_logs(logs)
+print(f"Parse {len(logs)} log entries")
+print(df.head())
+
+#Note : looking at the output , same ip adress shown 3 times through log 1 to 3 
+#same ip adress log 0 and 4 shown twice. indicates brute force 
+#Which IP addresses appear the most times ?
+ip_counts = df.groupby("ip")["raw_line"].count().reset_index() #group ip check line then count have it added to the new index
+ip_counts.columns = ["ip", "count"] #column names 
+ip_counts = ip_counts.sort_values("count", ascending=False) #sort ipcounts , sort counts from highest to lowest
+print(ip_counts)
+
+#Note:  Output shows that ip 91.240.118.172 has 200 attempts , 185.220.101.45 shows 114 attempts , 203.45.167.23 shows 106 attempts
+#indicating that a 3 were attempst of brute force by attacker 
+#Detection Log
+
+threshold = 10 # common threshold for 10 failed attempts.
+
+suspicious_ips = ip_counts.loc[ip_counts['count'] > threshold] 
+print(f"Suspicious IPs detected {suspicious_ips}")
+    
+
